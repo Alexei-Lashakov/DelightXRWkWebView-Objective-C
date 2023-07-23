ce8ec105868937faf621117abc230af2089df8da
Alex Lashakov <alexeilashakov@gmail.com>
2023-07-23 11:08:24 +0200

    add screen rotation

--- DelightXRWebView/ViewController.m
+++ DelightXRWebView/ViewController.m
@@ -27,4 +27,34 @@
     decisionHandler(WKNavigationActionPolicyAllow);
 }
 
+- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
+    [super traitCollectionDidChange:previousTraitCollection];
+
+    // Check if the device orientation has changed
+    if (self.traitCollection.verticalSizeClass != previousTraitCollection.verticalSizeClass ||
+        self.traitCollection.horizontalSizeClass != previousTraitCollection.horizontalSizeClass) {
+        [self updateWebViewConstraints];
+    }
+}
+
+- (void)updateWebViewConstraints {
+    // Remove existing constraints
+    [self.view removeConstraints:self.view.constraints];
+
+    // Set up new constraints based on the device orientation
+    if (self.traitCollection.verticalSizeClass == UIUserInterfaceSizeClassCompact) {
+        // Landscape mode: WKWebView fills the width and height of the view
+        [self.webView.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
+        [self.webView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
+        [self.webView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
+        [self.webView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
+    } else {
+        // Portrait mode: WKWebView fills the width and height of the view
+        [self.webView.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
+        [self.webView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
+        [self.webView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
+        [self.webView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
+    }
+}
+
 @end
